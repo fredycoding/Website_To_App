03ec2be65e781e63479983ad9d3544d8b0929f01
xbox360colombia <xbox360colombia@gmail.com>
2018-09-09 20:53:42 -0500

    Initial commit

---
+++ app/src/main/java/net/tutorialesbasicos/webview/MainActivity.java
@@ -0,0 +1,60 @@
+package net.tutorialesbasicos.webview;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webview);
+        // Configure related browser settings
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // Configure the client to use when opening URLs
+        myWebView.setWebViewClient(new MyBrowser());
+        // Load the initial URL
+        myWebView.loadUrl("https://google.com");
+    }
+
+    //Para cuando presiono el boton atras en una url para que no se salga.
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // Manages the behavior when URLs are loaded
+    private class MyBrowser extends WebViewClient {
+        @SuppressWarnings("deprecation")
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+    }
+
+}
+
